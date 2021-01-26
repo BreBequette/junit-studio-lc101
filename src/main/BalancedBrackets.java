0c5a5106a57896cfd04c3b4bf4ef93a29198aca1
Bre Bequette Reuther <67883123+BreBequette@users.noreply.github.com>
2021-01-25 20:16:00 -0600

    finished studio

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -21,15 +21,43 @@ public class BalancedBrackets {
      * @param str - to be validated
      * @return true if balanced, false otherwise
      */
+
+//  ORIGINAL CODE:
+//    public static boolean hasBalancedBrackets(String str) {
+//        int brackets = 0;
+//        for (char ch : str.toCharArray()) {
+//            if (ch == '[') {
+//                brackets++;
+//            } else if (ch == ']') {
+//                brackets--;
+//            }
+//        }
+//        return brackets == 0;
+//    }
+
     public static boolean hasBalancedBrackets(String str) {
         int brackets = 0;
+        char lastBracket = 'a';
         for (char ch : str.toCharArray()) {
             if (ch == '[') {
                 brackets++;
+                lastBracket = '[';
             } else if (ch == ']') {
-                brackets--;
+                if (lastBracket == '['){ //keep an even num of brackets
+                    brackets--;
+                } else if (brackets < 0){ //if a closing bracket doesn't follow open bracket, return false
+                    return false;
+                } else {
+                    brackets++;
+                }
+
             }
         }
-        return brackets == 0;
-    }
-}
+
+        if (brackets == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }//end hasBalancedBrackets method
+}//end BalancedBrackets class
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,80 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
 
+    //backwards brackets returns false
+    @Test
+    public void backwardsBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    //string enclosed in set of brackets returns true
+    @Test
+    public void strEnclosedInBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[According to all known laws of aviation, there is no way that a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyways.]"));
+    }
+
+    //string enclosed in backwards set of brackets returns false
+    @Test
+    public void strEnclosedInBackwardsBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]According to all known laws of aviation, there is no way that a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyways.["));
+    }
+
+    //string of only open bracket returns false
+    @Test
+    public void onlyOpenBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    //string of only closed bracket returns false
+    @Test
+    public void onlyClosedBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    //set of 2 open brackets returns false
+    @Test
+    public void twoOpenBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[["));
+    }
+
+    //set of 2 closed brackets returns false
+    @Test
+    public void twoClosedBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]"));
+    }
+
+    //set of brackets anywhere in string returns true
+    @Test
+    public void bracketsWithinStringReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("According to all known [laws of aviation], there is no way that a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyways."));
+    }
+
+    //uneven amount of open and closed brackets returns false
+    @Test
+    public void unevenBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[[[[][][]]]"));
+    }
+
+    //string with no brackets returns true
+    @Test
+    public void noBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("According to all known laws of aviation, there is no way that a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyways."));
+    }
+
+    //string with no closing bracket anywhere returns false
+    @Test
+    public void noClosingBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[According to all known laws of aviation, there is no way that a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyways."));
+    }
+
+    //string with no opening bracket anywhere returns false
+    @Test
+    public void noOpeningBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("According to all known laws of aviation, there is no way that a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyways.]"));
+    }
 }
